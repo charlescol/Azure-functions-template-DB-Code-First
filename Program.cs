035a03ac136ff416f50db47ddea8a841120e3c04
charlescol <charlescol@hotmail.fr>
2024-07-26 22:36:59 -0400

    fix: function startup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using Config.Database.Context;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,6 +10,19 @@ var host = new HostBuilder()
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        string SqlConnection = Environment.GetEnvironmentVariable("DefaultConnection");
+        if (SqlConnection != null )
+        {
+            var context = new UsersContext(new DbContextOptionsBuilder<UsersContext>().UseSqlServer(SqlConnection).Options);
+            services.AddTransient<Services.IUsersService<Models.Users>, Services.UsersService>((_) =>
+            {
+                return new Services.UsersService(context);
+            });
+        }
+        else
+        {
+            Console.WriteLine("DefaultConnection missing in environment variables");
+        }
     })
     .Build();
 
